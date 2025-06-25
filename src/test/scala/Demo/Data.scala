3648d417870f881b1bc55a29f24bcbdac89035a9
Jair Leal <100161561+JairLeal157@users.noreply.github.com>
2025-06-24 21:27:31 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -1,7 +1,7 @@
 package Demo
 
 object Data{
-    val url= "https://thinking-tester-contact-list.herokuapp.com/"
+    val url= "jairleal@email.com"
     val email = "test@testudeachris.com"
     val password = "12345678"
 }
