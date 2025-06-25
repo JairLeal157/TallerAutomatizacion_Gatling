39ccc87ffd6487c121caff7409adb3e63656e042
Jair Leal <100161561+JairLeal157@users.noreply.github.com>
2025-06-24 21:52:01 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -4,4 +4,5 @@ object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
     val email = "jairleal@email.com"
     val password = "12345678"
+    val token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODViNTEwNDE4MTQyNDAwMTUyYWVhMWYiLCJpYXQiOjE3NTA4MTk4NzJ9.jBjVPopy56H7PX4PILM2a9zCfpz_oMI0CYOAsFk0CXw"
 }
