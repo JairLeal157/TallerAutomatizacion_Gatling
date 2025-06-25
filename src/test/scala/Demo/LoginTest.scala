7b9a8eba080ec6bbaa697f061c2cfd4151e93cb2
Jair Leal <100161561+JairLeal157@users.noreply.github.com>
2025-06-24 21:38:40 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -21,9 +21,34 @@ class LoginTest extends Simulation{
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))
     )
+  // Scenario: Add Contact (includes login)
+  val scnAddContact = scenario("Add Contact")
+    .exec(
+      http("Create Contact")
+        .post("/contacts")
+        .header("Authorization", "Bearer ${authToken}")
+               .body(StringBody(
+          "{\n" +
+          "  \"firstName\": \"John\",\n" +
+          "  \"lastName\": \"Doe\",\n" +
+          "  \"birthdate\": \"1970-01-01\",\n" +
+          "  \"email\": \"jdoe@fake.com\",\n" +
+          "  \"phone\": \"8005555555\",\n" +
+          "  \"street1\": \"1 Main St.\",\n" +
+          "  \"street2\": \"Apartment A\",\n" +
+          "  \"city\": \"Anytown\",\n" +
+          "  \"stateProvince\": \"KS\",\n" +
+          "  \"postalCode\": \"12345\",\n" +
+          "  \"country\": \"USA\"\n" +
+          "}"
+        )).asJson
+        .check(status.is(201)
+    )
+
 
   // 3 Load Scenario
   setUp(
-    scn.inject(rampUsersPerSec(5).to(15).during(30))
+    scn.inject(rampUsersPerSec(5).to(15).during(30)),
+    scnAddContact.inject(rampUsersPerSec(5).to(15).during(30.seconds))
   ).protocols(httpConf);
 }
