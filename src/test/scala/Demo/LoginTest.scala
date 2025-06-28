7bee4b3d0d2c82c8f8ad762e9a0f70ebbef3d927
Jair Leal <100161561+JairLeal157@users.noreply.github.com>
2025-06-28 13:58:33 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -3,7 +3,6 @@ package Demo
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import Demo.Data._
-import scala.concurrent.duration._        // <= necesario para .seconds
 
 class LoginTest extends Simulation{
 
@@ -11,45 +10,27 @@ class LoginTest extends Simulation{
   val httpConf = http.baseUrl(url)
     .acceptHeader("application/json")
     //Verificar de forma general para todas las solicitudes
-    .check(status.in(200, 201))
 
   // 2 Scenario Definition
-  val scn = scenario("Login").
+  val scn = scenario("LoginAnd").
     exec(http("login")
       .post(s"users/login")
       .body(StringBody(s"""{"email": "$email", "password": "$password"}""")).asJson
-       //Recibir información de la cuenta
+         //Validar status 200 del servicio
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))
     )
-  // Scenario: Add Contact (includes login)
-  val scnAddContact = scenario("Add Contact")
     .exec(
       http("Create Contact")
-        .post("/contacts")
-        .header("Authorization", s"Bearer ${Data.token}")
-        .body(StringBody(
-          "{\n" +
-          "  \"firstName\": \"John\",\n" +
-          "  \"lastName\": \"Doe\",\n" +
-          "  \"birthdate\": \"1970-01-01\",\n" +
-          "  \"email\": \"jdoe@fake.com\",\n" +
-          "  \"phone\": \"8005555555\",\n" +
-          "  \"street1\": \"1 Main St.\",\n" +
-          "  \"street2\": \"Apartment A\",\n" +
-          "  \"city\": \"Anytown\",\n" +
-          "  \"stateProvince\": \"KS\",\n" +
-          "  \"postalCode\": \"12345\",\n" +
-          "  \"country\": \"USA\"\n" +
-          "}"
-        )).asJson
+        .post(s"contacts")
+        .header("Authorization", "Bearer ${authToken}")
+        .body(StringBody(s"""{"firstName": "Jair Santiago","lastName": "Leal Miranda","birthdate": "2002-10-07","email": "jair@email.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
         .check(status.is(201))
     )
 
 
   // 3 Load Scenario
   setUp(
-    scn.inject(rampUsersPerSec(5).to(15).during(30)),
-    scnAddContact.inject(rampUsersPerSec(5).to(15).during(30.seconds))
+    scn.inject(rampUsers(10).during(50))
   ).protocols(httpConf);
 }
